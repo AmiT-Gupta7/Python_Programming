acebda401aa3c42189187622e7f2608c5b3d6eb7
AmiT-Gupta7 <guptaamit61739@gmail.com>
2025-08-07 21:58:37 +0530

    Resolved merge conflicts

---
+++ list_p1.py
@@ -0,0 +1,23 @@
+list1 = [1, 3, 5, 2, 7, 4, 8]
+list2 = ["hello", "apple", "welcome"]
+
+# Pre-define Method on List...
+
+list1.append(9)
+print(list1)
+
+list1.reverse()
+print(list1)
+
+list2.sort()
+print(list2)
+
+list1.sort(reverse=True)
+print(list1)
+
+list1.remove(4)
+print(list1)
+
+list1.pop(5)
+print(list1)
+
---
+++ str_method.py
@@ -0,0 +1,5 @@
+string = "hello this is my first string program"
+
+print(len(string))
+print(string.capitalize())
+print(string.count("i"))
