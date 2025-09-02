3a9ac5550d4d89b2bd8dd526f1c6d63e7257e72f
AmiT-Gupta7 <guptaamit61739@gmail.com>
2025-09-03 00:17:28 +0530

    Learning Basic of Dictionary

--- dictionary.py
+++ dictionary.py
@@ -5,10 +5,21 @@ my_dict = {"name": "Amit", "age": 21, "city": "gkp"}
 # print(my_dict.items())
 # print(my_dict.get("age"))
 
-my_dict.update({"country": "india",})
-print(my_dict)
+# my_dict.update({"country": "india",})
+# print(my_dict)
 
-my_dict.update({"city":"delhi"})
+# my_dict.update({"city":"delhi"})
 
-# my_dict.pop("city")
-print(my_dict)
+# # my_dict.pop("city")
+# print(my_dict)
+
+my_dict2 ={}
+
+n = int(input("Enter number of subjects: "))
+for i in range(n):
+    subjet = input("Enter your subject: ")
+    marks = float(input("Enter your marks: "))
+    
+    my_dict2.update({subjet:marks})
+print(my_dict2)
+
