35b99384a9e9075ec722b96384700f3f10c2e7a6
AmiT-Gupta7 <guptaamit61739@gmail.com>
2025-09-02 23:51:58 +0530

    Learning Basic of set

---
+++ set.py
@@ -0,0 +1,24 @@
+collection = set() # Create an empty set
+
+# Add element to the set
+collection.add(1)
+collection.add(2)
+collection.add(3)
+collection.add(2) # Duplicate, will not be added
+print(collection)
+
+# Remove element from the set
+collection.remove(2)
+print(collection)
+
+# Check if an element is in the set
+print(3 in collection)
+print(2 in collection)
+
+# Iterate through the set
+for item in collection:
+    print(item)
+
+# Clear the set
+collection.clear()
+print(collection)
